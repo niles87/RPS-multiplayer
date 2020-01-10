142aad8d390e11c6d94d54cc71234b293e96766c
niles87 <nbing19@gmail.com>
2020-01-10 06:16:43 -0800

    added a variable to check if there is a player already, recoded the first database value change event to reference playerone, added another database value change event to reference playertwo, changed the name event listener to check if there theres a playerone

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ import { config } from "./keys.js";
 firebase.initializeApp(config);
 
 // initial global variables
+var isTherePlayer1 = false;
 var player1;
 var player2;
 var playerOneWins = 0;
@@ -12,13 +13,32 @@ var playerTwoLoses = 0;
 var chatArr = [];
 var database = firebase.database();
 
-database.ref().on(
+database.ref("playerone").on(
   "value",
   function(data) {
+    console.log("on value change for playerone is working");
     if (data.child("firstPlayerName").exists()) {
       player1 = data.val().firstPlayerName;
+      console.log("--below is player one--");
       console.log(player1);
       $("#first-player").text(player1);
+      isTherePlayer1 = true;
+      $(".login").hide();
+    }
+  },
+  function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  }
+);
+database.ref("playertwo").on(
+  "value",
+  function(data) {
+    if (data.child("secondPlayerName").exists()) {
+      player2 = data.val().secondPlayerName;
+      console.log("--below is player two--");
+      console.log(player2);
+      $("#second-player").text(player2);
+      $(".login").hide();
     }
   },
   function(errorObject) {
@@ -28,15 +48,32 @@ database.ref().on(
 
 $("#name").on("click", function(event) {
   event.preventDefault();
-  var firstPlayer = $("#player-name")
-    .val()
-    .trim();
-  console.log(firstPlayer);
-  database.ref().set({
-    firstPlayerName: firstPlayer,
-  });
+  if (isTherePlayer1 === false) {
+    isTherePlayer1 = true;
+    var firstPlayer = $("#player-name")
+      .val()
+      .trim();
+    console.log(firstPlayer);
+    database.ref("playerone").set({
+      firstPlayerName: firstPlayer,
+    });
 
-  $("#first-player").text(firstPlayer);
+    $("#first-player").text(firstPlayer);
+    $(".login").hide();
+    var waitingFor = $("<p>Waiting for another player</p>");
+    $("#choices").append(waitingFor);
+  } else {
+    var secondPlayer = $("#player-name")
+      .val()
+      .trim();
+    console.log(secondPlayer);
+    database.ref("playertwo").set({
+      secondPlayerName: secondPlayer,
+    });
+    $("#second-player").text(secondPlayer);
+    $(".login").hide();
+    $("#choices").empty();
+  }
   $("#player-name").val("");
 });
 
