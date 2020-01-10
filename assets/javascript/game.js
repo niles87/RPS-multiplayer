3a30147db67ab8eb56f09188e2a9a83050a727f2
niles87 <nbing19@gmail.com>
2020-01-09 19:19:12 -0800

    imported a file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,10 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyDB75Vy1rhvgCXROeXxbWEAZPQtvWEmyzg",
-  authDomain: "rockpaperscissorsduel.firebaseapp.com",
-  databaseURL: "https://rockpaperscissorsduel.firebaseio.com",
-  projectId: "rockpaperscissorsduel",
-  storageBucket: "rockpaperscissorsduel.appspot.com",
-  messagingSenderId: "114611548615",
-  appId: "1:114611548615:web:7ed324223b2e21faa729e2",
-  measurementId: "G-43T9VGQG7N",
-};
+import { config } from "./keys.js";
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 
 // initial global variables
-var player1 = "";
-var player2 = "";
+var player1;
+var player2;
 var playerOneWins = 0;
 var playerOneLoses = 0;
 var playerTwoWins = 0;
@@ -26,6 +16,9 @@ database.ref().on(
   "value",
   function(data) {
     if (data.child("firstPlayerName").exists()) {
+      player1 = data.val().firstPlayerName;
+      console.log(player1);
+      $("#first-player").text(player1);
     }
   },
   function(errorObject) {
